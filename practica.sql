862587db5ce6837df5c81d8af3ab0a14c0d7c8d0
CharlyHer99 <100860485+CharlyHer99@users.noreply.github.com>
2024-06-27 14:16:40 -0600

    Add files via upload

--- practica.sql
+++ practica.sql
@@ -1,3 +1,6 @@
+USE MASTER
+GO
+
 CREATE DATABASE practica
 GO
 
@@ -26,7 +29,6 @@ INSERT INTO vendedores VALUES
 ('Ramón', 'Morelos #812', 36400, 1), ('Juan', 'Madero #321', 36138, 1), ('Jesús', 'Pedro Moreno #123', 36123, 2)
 GO
 
-
 CREATE TABLE clientes(
 idCliente int not null primary key identity(1,1),
 nombre varchar(50),
@@ -41,9 +43,9 @@ GO
 INSERT INTO clientes VALUES
 ('Carlos Hernández', 'Gardenia #168', 1, 36416, 'Purísima', 1), 
 ('Miguel Hernández', 'Gardenia #168', 1, 36416, 'Purísima', 4),
-('´Ramiro Rendon', 'Morelos #16', 1, 36400, 'Purísima', 5),
+('´Ramiro Rendon', 'Morelos #16', 0, 36400, 'Purísima', 5),
 ('Paulina Guerrero', 'Veneros #132', 1, 35600, 'León', 1),
-('Bruno Pinilla', 'Torres #220', 1, 35400, 'Silao', 2)
+('Bruno Pinilla', 'Torres #220', 0, 35400, 'Silao', 2)
 GO
 
 CREATE TABLE articulos(
@@ -54,9 +56,10 @@ stock int,
 stockMinimo int
 )
 GO
-INSERT INTO articulos VALUES
+INSERT INTO articulos VALUES 
 ('Pelota', 12.00, 50, 30), ('Balón', 50.00, 30, 30), ('Guantes', 30.00, 20, 20),
-('Tachones', 200.00, 50, 30), ('Mochila', 70.00, 40, 40), ('Gorra', 40.00, 20, 10)
+('Tachones', 200.00, 50, 30), ('Mochila', 70.00, 40, 40), ('Gorra', 40.00, 20, 10),
+('pulsera', 5.00, 5, 10)
 GO
 
 CREATE TABLE facturas(
@@ -74,7 +77,8 @@ GO
 INSERT INTO facturas VALUES
 (2000.00, '2024-05-20', 1, 1, 20.00, 10), (1500.00, '2024-04-28', 2, 4, 20.00, 5),
 (300.00, '2024-01-10', 3, 5, 20.00, 15), (23300.00, '2024-05-12', 4, 1, 20.00, 30),
-(2900.00, '2024-03-07', 5, 2, 20.00, 9), (12900.00, '2024-02-16', 1, 1, 20.00, 20)
+(2900.00, '2024-03-07', 5, 2, 20.00, 9), (12900.00, '2024-02-16', 1, 1, 20.00, 20),
+(20.00, '2024-02-16', 2, 4, 20.00, 5)
 GO
 
 CREATE TABLE detalleFacturas(
@@ -93,7 +97,88 @@ INSERT INTO detalleFacturas VALUES
 (3, 1, 12, 300), 
 (4, 1, 150, 1800), (4, 2, 15, 2500), (4, 3, 150, 4500),(4, 4, 30, 6000),  (4, 5, 25, 3500), (4, 6, 200, 5000),
 (5, 1, 75, 900), (5, 2, 20, 1000), (5, 4, 5, 1000), 
-(6, 1, 100, 1200), (6, 2, 20, 1000), (6, 3, 100, 3000), (6, 4, 15, 3000),  (6, 5, 10, 700), (6, 6, 100, 4000)
+(6, 1, 100, 1200), (6, 2, 20, 1000), (6, 3, 100, 3000), (6, 4, 15, 3000),  (6, 5, 10, 700), (6, 6, 100, 4000),
+(7, 7, 4, 20)
+GO
 
 
 
+
+--ejercicio 3
+CREATE PROCEDURE spListarFacturasPorAño
+	@date varchar(4)
+AS
+	SELECT * from facturas WHERE fecha like CONCAT(@date,'%')
+GO
+EXEC spListarFacturasPorAño '2024'
+GO
+
+--ejercicio #4
+SELECT v.*,
+	(SELECT COUNT(idCliente) 
+		FROM clientes 
+		WHERE vendedorActual = v.idVendedor) 
+	AS clientesAsignados
+from vendedores v
+GO
+
+--ejercicio 5
+CREATE PROCEDURE spClinteVendedorCiudad
+	-- 0 = inactivo, 1 = activo, 2 = todos
+	@estatus int
+AS
+	IF @estatus < 2
+		SELECT c.* from clientes c 
+		WHERE estatus = @estatus
+	ELSE
+		SELECT * FROM clientes
+GO
+EXEC spClinteVendedorCiudad 1
+GO
+
+
+--ejercicio 6
+CREATE FUNCTION rankingArticulos()
+	RETURNS TABLE
+AS
+	RETURN
+			SELECT SUM(df.cantidadSolicitada) cantidadVendida, df.idProducto, 
+				a.precio, (SELECT fecha FROM facturas WHERE idFactura =
+							(SELECT MAX(idFactura) from detalleFacturas WHERE idProducto = a.idArticulo)) AS ultimaFechaVendida,
+				ROW_NUMBER() OVER (ORDER BY SUM(df.cantidadSolicitada) DESC) AS ranking
+			FROM detalleFacturas df
+			INNER JOIN articulos as a ON df.idProducto = a.idArticulo
+			GROUP BY df.idProducto, a.precio, a.idArticulo
+GO
+SELECT * from dbo.rankingArticulos() 
+GO
+
+
+--ejercicio 7
+CREATE VIEW articuloStockMenorAlMinimo
+AS
+	SELECT a.*,(SELECT fecha FROM facturas WHERE idFactura =
+					(SELECT MAX(idFactura) from detalleFacturas WHERE idProducto = a.idArticulo)) AS ultimaFechaVendida,
+					(SELECT ranking from dbo.rankingArticulos() WHERE idProducto = a.idArticulo) AS ranking
+	FROM articulos a
+	RIGHT JOIN detalleFacturas df ON a.idArticulo = df.idProducto
+	LEFT JOIN facturas f ON df.idFactura = f.idFactura 
+		WHERE a.stock < a.stockMinimo
+GO
+SELECT * from articuloStockMenorAlMinimo
+GO
+
+--ejercicio 8
+CREATE FUNCTION obtenerPromedioImportePorVendedor
+	(
+		@idVendedor int, @mes varchar(2)
+	) 
+	RETURNS DECIMAL(10, 2)
+AS
+BEGIN
+	RETURN 
+		(SELECT CONVERT(DECIMAL(10,2), AVG(importe)) promedio from facturas WHERE idVendedor = 1 AND fecha like CONCAT('%-',@mes,'-%'))
+END
+GO
+SELECT dbo.obtenerPromedioImportePorVendedor(1, '02') as promedio
+GO
